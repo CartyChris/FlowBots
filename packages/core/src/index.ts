a67c1d9a4f3fee1027c06f1b7cb8aacda1d2c576
CartyChris <chriscarty123@gmail.com>
2026-08-16 04:43:34 -0400

    feat(core): export persona and social engines

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,6 +3,8 @@ export * from "./async.js";
 export * from "./cron.js";
 export * from "./events.js";
 export * from "./message-pages.js";
+export * from "./personality.js";
 export * from "./run-state.js";
 export * from "./secrets-guard.js";
 export * from "./signup-policy.js";
+export * from "./social.js";
