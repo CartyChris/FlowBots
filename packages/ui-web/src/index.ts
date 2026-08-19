01e2a5a9a6e35456ae2a4dd832a3f1f8b2d1a52b
CartyChris <chriscarty123@gmail.com>
2026-08-18 22:31:31 -0400

    feat(ui): export bot face choices

--- packages/ui-web/src/index.ts
+++ packages/ui-web/src/index.ts
@@ -1,3 +1,10 @@
-export { BotAvatar, Wordmark } from "./bot-avatar.js";
+export {
+  avatarVariantForColor,
+  BotAvatar,
+  BOT_AVATAR_FACE_CHOICES,
+  BOT_AVATAR_VARIANTS,
+  Wordmark,
+} from "./bot-avatar.js";
+export type { BotAvatarState, BotAvatarVariant } from "./bot-avatar.js";
 export { Button, buttonVariants } from "./button.js";
 export { cn } from "./lib/utils.js";
