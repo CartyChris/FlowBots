1bfe3c02cd5e0a4f630db52bbf3b10da58d70dd5
CartyChris <chriscarty123@gmail.com>
2026-08-18 22:37:40 -0400

    style(ui): apply Biome export order

--- packages/ui-web/src/index.ts
+++ packages/ui-web/src/index.ts
@@ -1,10 +1,10 @@
+export type { BotAvatarState, BotAvatarVariant } from "./bot-avatar.js";
 export {
   avatarVariantForColor,
-  BotAvatar,
   BOT_AVATAR_FACE_CHOICES,
   BOT_AVATAR_VARIANTS,
+  BotAvatar,
   Wordmark,
 } from "./bot-avatar.js";
-export type { BotAvatarState, BotAvatarVariant } from "./bot-avatar.js";
 export { Button, buttonVariants } from "./button.js";
 export { cn } from "./lib/utils.js";
