bfd915cc0b538f93df2cc58fe8917c93ac3e8bba
CartyChris <chriscarty123@gmail.com>
2026-08-26 02:13:34 -0400

    feat: expose flow awareness to adapters

---
+++ packages/adapters/src/flow-awareness.ts
@@ -0,0 +1,11 @@
+export {
+  applyFlowMembership,
+  botParticipatesInFlow,
+  buildFlowRoster,
+  flowAwarenessInstruction,
+  flowMembershipFromInstructions,
+  type FlowBotSummary,
+  type FlowMembership,
+  type FlowRoster,
+  type FlowRosterMember,
+} from "@rakazo/core";
